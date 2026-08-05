2ce26489d5b20222dcc7f16c8ba5183c526252ca
Sebastian Buitrago <sebastianbbe@gmail.com>
2026-08-05 16:53:24 -0500

    Migración de SQLLite a Oracle 21c

--- database.py
+++ database.py
@@ -4,9 +4,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 BASE_DIR = Path(__file__).resolve().parent
-DATABASE_URL = f"sqlite:///{BASE_DIR / 'usuarios.db'}"
+# DATABASE_URL = f"sqlite:///{BASE_DIR / 'usuarios.db'}"
+DATABASE_URL = "oracle+oracledb://SISTCLIE:NuevaClave@192.168.1.73:1521/?service_name=xepdb1"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
--- models/user.py
+++ models/user.py
@@ -1,12 +1,17 @@
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Identity
 from ..database import Base
 
 class UserDB(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    dni = Column(String, nullable=False, unique=True, index=True)
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
+    __tablename__ = "app_users"
+    id = Column(
+        Integer,
+        Identity(start=1, increment=1),
+        primary_key=True,
+    )
+    dni = Column(String(20), nullable=False, unique=True, index=True)
+    name = Column(String(100), index=True)
+    email = Column(String(255), unique=True, index=True)
     status = Column(Boolean, default=True)
-    phone = Column(String, nullable=True)
+    phone = Column(String(20), nullable=True)
     password = Column(String(200), nullable=False)
