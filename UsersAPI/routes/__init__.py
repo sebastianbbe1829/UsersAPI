d6209ebe39a15fc534334ab64783907072c0e2b3
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-08-22 16:18:52 -0500

    feat: register super auth routes

--- UsersAPI/routes/__init__.py
+++ UsersAPI/routes/__init__.py
@@ -1,5 +1,6 @@
 from .user_routes import user_routes
 from .auth_routes import auth_routers
+from .global_auth_routes import global_auth_routes
 from .tenant_routes import tenant_routes
 from .user_tenant_routes import user_tenant_routes
 from .role_routes import role_routes
@@ -12,6 +13,7 @@ from .permission_routes import permission_routes
 __all__ = [
     "user_routes",
     "auth_routers",
+    "global_auth_routes",
     "tenant_routes",
     "user_tenant_routes",
     "role_routes",
