9ff923fbb109e36008e70631215ab160c86d93b1
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-08-22 16:18:37 -0500

    fix: preserve existing schema exports

--- UsersAPI/schemas/__init__.py
+++ UsersAPI/schemas/__init__.py
@@ -1,3 +1,45 @@
+from .user import (
+    UserCreate,
+    UserDeleteResponse,
+    UserRead,
+    UserUpdate,
+    UserActivateResponse,
+)
+
+from .tenant import (
+    TenantCreate,
+    TenantDeleteResponse,
+    TenantRead,
+    TenantUpdate,
+    BootstrapRequest,
+    BootstrapResponse,
+)
+
+from .user_tenant import (
+    UserTenantCreate,
+    UserTenantRead,
+    UserTenantDeleteResponse,
+)
+
+from .role import (
+    RoleCreate,
+    RoleDeleteResponse,
+    RoleRead,
+    RoleUpdate,
+)
+
+from .user_tenant_role import (
+    UserTenantRoleCreate,
+    UserTenantRoleRead,
+    UserTenantRoleDeleteResponse,
+)
+
+from .role_permission import (
+    RolePermissionCreate,
+    RolePermissionRead,
+    RolePermissionDeleteResponse,
+)
+
 from .auth import (
     LoginRequest,
     LoginResponse,
@@ -5,21 +47,46 @@ from .auth import (
     TokenTenantResponse,
     TokenValidationResponse,
 )
+
 from .global_auth import (
     SuperBootstrapRequest,
     SuperBootstrapResponse,
     SuperLoginRequest,
     SuperLoginResponse,
 )
-from .permission import *
-from .role import *
-from .role_permission import *
-from .tenant import *
-from .user import *
-from .user_tenant import *
-from .user_tenant_role import *
+
+from .permission import (
+    PermissionCreate,
+    PermissionRead,
+    PermissionResponse,
+)
 
 __all__ = [
+    "PermissionCreate",
+    "PermissionRead",
+    "PermissionResponse",
+    "UserCreate",
+    "UserDeleteResponse",
+    "UserRead",
+    "UserUpdate",
+    "UserActivateResponse",
+    "TenantCreate",
+    "TenantDeleteResponse",
+    "TenantRead",
+    "TenantUpdate",
+    "UserTenantCreate",
+    "UserTenantRead",
+    "UserTenantDeleteResponse",
+    "RoleCreate",
+    "RoleDeleteResponse",
+    "RoleRead",
+    "RoleUpdate",
+    "UserTenantRoleCreate",
+    "UserTenantRoleRead",
+    "UserTenantRoleDeleteResponse",
+    "RolePermissionCreate",
+    "RolePermissionRead",
+    "RolePermissionDeleteResponse",
     "LoginRequest",
     "LoginResponse",
     "TokenUserResponse",
@@ -29,4 +96,6 @@ __all__ = [
     "SuperBootstrapResponse",
     "SuperLoginRequest",
     "SuperLoginResponse",
+    "BootstrapRequest",
+    "BootstrapResponse",
 ]
