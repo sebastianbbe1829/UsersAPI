7466bbac9dc7dad411039113203d4ac8fed683ef
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-08-31 16:57:38 -0500

    feat: register extinguisher routes

--- UsersAPI/routes/__init__.py
+++ UsersAPI/routes/__init__.py
@@ -11,20 +11,11 @@ from .role_permission_routes import role_permission_routes
 from .bootstrap_tenant_routes import bootstrap_tenant_routes
 from .permission_routes import permission_routes
 from .email_routes import email_routes
-
+from .extinguisher_routes import extinguisher_routes
 
 __all__ = [
-    "user_routes",
-    "auth_routers",
-    "global_auth_routes",
-    "tenant_routes",
-    "tenant_config_routes",
-    "tenant_config_public_routes",
-    "user_tenant_routes",
-    "role_routes",
-    "user_tenant_role_routes",
-    "role_permission_routes",
-    "bootstrap_tenant_routes",
-    "permission_routes",
-    "email_routes",
+    "user_routes", "auth_routers", "global_auth_routes", "tenant_routes",
+    "tenant_config_routes", "tenant_config_public_routes", "user_tenant_routes",
+    "role_routes", "user_tenant_role_routes", "role_permission_routes",
+    "bootstrap_tenant_routes", "permission_routes", "email_routes", "extinguisher_routes",
 ]
