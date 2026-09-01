7fcfd545150f4181d4cf3535c5f772134de473d8
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-01 13:41:20 -0500

    refactor: register OTP controller exports

--- UsersAPI/controllers/__init__.py
+++ UsersAPI/controllers/__init__.py
@@ -15,6 +15,7 @@ from .extinguisher_inspection_item_controller import (
     actualizar_item_revision,
     desactivar_item_revision,
 )
+from .otp_controller import create_otp, verify_otp, validate_otp_api_key
 
 __all__ = [
     "listar_permisos", "obtener_permiso", "create_user", "list_users", "export_users", "get_user", "update_user",
@@ -22,5 +23,6 @@ __all__ = [
     "eliminar_tenant", "listar_mis_tenants", "listar_roles", "obtener_rol", "actualizar_rol", "eliminar_rol", "crear_rol",
     "listar_roles_usuario", "eliminar_rol_usuario", "asignar_rol_usuario", "asignar_permiso_rol", "listar_permisos_rol",
     "eliminar_permiso_rol", "activate_user", "bootstrap_tenant_application", "listar_items_revision", "obtener_item_revision",
-    "crear_item_revision", "actualizar_item_revision", "desactivar_item_revision",
+    "crear_item_revision", "actualizar_item_revision", "desactivar_item_revision", "create_otp", "verify_otp",
+    "validate_otp_api_key",
 ]
