c8d436544bd2a72ef8f7f637fc89c4db12441cdf
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-08-31 20:20:02 -0500

    feat: export extinguisher type controller

--- UsersAPI/controllers/__init__.py
+++ UsersAPI/controllers/__init__.py
@@ -1,82 +1,11 @@
-from .user_controller import (
-    create_user,
-    export_users,
-    get_user,
-    list_users,
-    update_user,
-    activate_user,
-)
-
-from .auth_controller import (
-    get_current_user,
-    get_password_hash,
-)
-
-from .tenant_controller import (
-    crear_tenant,
-    listar_tenants,
-    obtener_tenant,
-    actualizar_tenant,
-    eliminar_tenant,
-    listar_mis_tenants,
-)
-
-from .role_controller import (
-    listar_roles,
-    obtener_rol,
-    actualizar_rol,
-    eliminar_rol,
-    crear_rol,
-)
-
-from .user_tenant_role_controller import (
-    listar_roles_usuario,
-    eliminar_rol_usuario,
-    asignar_rol_usuario,
-)
-
-from .role_permission_controller import (
-    asignar_permiso_rol,
-    listar_permisos_rol,
-    eliminar_permiso_rol,
-)
-
-from .bootstrap_tenant_controller import (
-    bootstrap_tenant_application,
-)
-
-from .permission_controller import (
-    listar_permisos,
-    obtener_permiso,
-)
-
-__all__ = [
-    "listar_permisos",
-    "obtener_permiso",
-    "create_user",
-    "list_users",
-    "export_users",
-    "get_user",
-    "update_user",
-    "get_current_user",
-    "get_password_hash",
-    "crear_tenant",
-    "listar_tenants",
-    "obtener_tenant",
-    "actualizar_tenant",
-    "eliminar_tenant",
-    "listar_mis_tenants",
-    "listar_roles",
-    "obtener_rol",
-    "actualizar_rol",
-    "eliminar_rol",
-    "crear_rol",
-    "listar_roles_usuario",
-    "eliminar_rol_usuario",
-    "asignar_rol_usuario",
-    "asignar_permiso_rol",
-    "listar_permisos_rol",
-    "eliminar_permiso_rol",
-    "activate_user",
-    "bootstrap_tenant_application",
-]
+from .user_controller import *
+from .tenant_controller import *
+from .tenant_config_controller import *
+from .user_tenant_controller import *
+from .role_controller import *
+from .user_tenant_role_controller import *
+from .role_permission_controller import *
+from .permission_controller import *
+from .bootstrap_tenant_controller import *
+from .extinguisher_controller import *
+from .extinguisher_type_controller import *
