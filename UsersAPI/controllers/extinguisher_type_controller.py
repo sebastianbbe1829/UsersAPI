a0615830f70f9b1e6c952df072ac2645a6d29101
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-08-31 20:45:54 -0500

    feat: add extinguisher type update and delete controller

--- UsersAPI/controllers/extinguisher_type_controller.py
+++ UsersAPI/controllers/extinguisher_type_controller.py
@@ -1,7 +1,12 @@
 from sqlalchemy.orm import Session
 
-from ..schemas import ExtinguisherTypeCreate
-from ..services.extinguisher_type_service import create_extinguisher_type, list_extinguisher_types
+from ..schemas import ExtinguisherTypeCreate, ExtinguisherTypeUpdate
+from ..services.extinguisher_type_service import (
+    create_extinguisher_type,
+    delete_extinguisher_type,
+    list_extinguisher_types,
+    update_extinguisher_type,
+)
 
 
 def listar_tipos_extintor(db: Session):
@@ -10,3 +15,11 @@ def listar_tipos_extintor(db: Session):
 
 def crear_tipo_extintor(datos: ExtinguisherTypeCreate, db: Session):
     return create_extinguisher_type(datos, db)
+
+
+def actualizar_tipo_extintor(type_id: int, datos: ExtinguisherTypeUpdate, db: Session):
+    return update_extinguisher_type(type_id, datos, db)
+
+
+def eliminar_tipo_extintor(type_id: int, db: Session):
+    return delete_extinguisher_type(type_id, db)
