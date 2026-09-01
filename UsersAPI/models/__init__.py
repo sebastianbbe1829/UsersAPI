076a7529217cb71bc8434ef5f9d2eaee8c9a00cd
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-08-31 20:19:12 -0500

    feat: export extinguisher type model

--- UsersAPI/models/__init__.py
+++ UsersAPI/models/__init__.py
@@ -8,21 +8,13 @@ from .permission import PermissionDB
 from .role_permission import RolePermissionDB
 from .user_tenant_role import UserTenantRoleDB
 from .extinguisher import ExtinguisherDB
+from .extinguisher_type import ExtinguisherTypeDB
 from .extinguisher_inspection import ExtinguisherInspectionDB, ExtinguisherInspectionItemDB
 from .extinguisher_recharge import ExtinguisherRechargeDB
 
 __all__ = [
-    "UserDB",
-    "TenantDB",
-    "TenantConfigDB",
-    "UserTenantDB",
-    "GlobalUserDB",
-    "RoleDB",
-    "PermissionDB",
-    "RolePermissionDB",
-    "UserTenantRoleDB",
-    "ExtinguisherDB",
-    "ExtinguisherInspectionDB",
-    "ExtinguisherInspectionItemDB",
+    "UserDB", "TenantDB", "TenantConfigDB", "UserTenantDB", "GlobalUserDB", "RoleDB",
+    "PermissionDB", "RolePermissionDB", "UserTenantRoleDB", "ExtinguisherDB",
+    "ExtinguisherTypeDB", "ExtinguisherInspectionDB", "ExtinguisherInspectionItemDB",
     "ExtinguisherRechargeDB",
 ]
