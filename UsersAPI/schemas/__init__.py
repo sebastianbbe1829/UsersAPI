54fae8e8a72bf22c146f511c7fa10ca64d56b3e5
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-08-31 20:19:58 -0500

    feat: export extinguisher type schemas

--- UsersAPI/schemas/__init__.py
+++ UsersAPI/schemas/__init__.py
@@ -1,123 +1,21 @@
-from .user import (
-    UserCreate,
-    UserDeleteResponse,
-    UserRead,
-    UserUpdate,
-    UserActivateResponse,
-)
-
-from .tenant import (
-    TenantCreate,
-    TenantDeleteResponse,
-    TenantRead,
-    TenantUpdate,
-    BootstrapTenantRequest,
-    BootstrapTenantResponse,
-)
-
-from .tenant_config import (
-    TenantConfigUpdate,
-    TenantConfigRead,
-)
-
-from .user_tenant import (
-    UserTenantCreate,
-    UserTenantRead,
-    UserTenantDeleteResponse,
-)
-
-from .role import (
-    RoleCreate,
-    RoleDeleteResponse,
-    RoleRead,
-    RoleUpdate,
-)
-
-from .user_tenant_role import (
-    UserTenantRoleCreate,
-    UserTenantRoleRead,
-    UserTenantRoleDeleteResponse,
-)
-
-from .role_permission import (
-    RolePermissionCreate,
-    RolePermissionRead,
-    RolePermissionDeleteResponse,
-)
-
-from .auth import (
-    LoginRequest,
-    LoginResponse,
-    TokenUserResponse,
-    TokenTenantResponse,
-    TokenValidationResponse,
-)
-
-from .global_auth import (
-    SuperBootstrapMfaVerifyRequest,
-    SuperBootstrapMfaVerifyResponse,
-    SuperBootstrapRequest,
-    SuperBootstrapResponse,
-    SuperLoginRequest,
-    SuperLoginResponse,
-)
-
-from .permission import (
-    PermissionCreate,
-    PermissionRead,
-    PermissionResponse,
-)
-
-from .extinguisher import (
-    ExtinguisherCreate,
-    ExtinguisherDeleteResponse,
-    ExtinguisherRead,
-    ExtinguisherUpdate,
-)
+from .user import UserCreate, UserDeleteResponse, UserRead, UserUpdate, UserActivateResponse
+from .tenant import TenantCreate, TenantDeleteResponse, TenantRead, TenantUpdate, BootstrapTenantRequest, BootstrapTenantResponse
+from .tenant_config import TenantConfigUpdate, TenantConfigRead
+from .user_tenant import UserTenantCreate, UserTenantRead, UserTenantDeleteResponse
+from .role import RoleCreate, RoleDeleteResponse, RoleRead, RoleUpdate
+from .user_tenant_role import UserTenantRoleCreate, UserTenantRoleRead, UserTenantRoleDeleteResponse
+from .role_permission import RolePermissionCreate, RolePermissionRead, RolePermissionDeleteResponse
+from .auth import LoginRequest, LoginResponse, TokenUserResponse, TokenTenantResponse, TokenValidationResponse
+from .global_auth import SuperBootstrapMfaVerifyRequest, SuperBootstrapMfaVerifyResponse, SuperBootstrapRequest, SuperBootstrapResponse, SuperLoginRequest, SuperLoginResponse
+from .permission import PermissionCreate, PermissionRead, PermissionResponse
+from .extinguisher import ExtinguisherCreate, ExtinguisherDeleteResponse, ExtinguisherRead, ExtinguisherUpdate
+from .extinguisher_type import ExtinguisherTypeCreate, ExtinguisherTypeRead
 
 __all__ = [
-    "PermissionCreate",
-    "PermissionRead",
-    "PermissionResponse",
-    "UserCreate",
-    "UserDeleteResponse",
-    "UserRead",
-    "UserUpdate",
-    "UserActivateResponse",
-    "TenantCreate",
-    "TenantDeleteResponse",
-    "TenantRead",
-    "TenantUpdate",
-    "TenantConfigUpdate",
-    "TenantConfigRead",
-    "UserTenantCreate",
-    "UserTenantRead",
-    "UserTenantDeleteResponse",
-    "RoleCreate",
-    "RoleDeleteResponse",
-    "RoleRead",
-    "RoleUpdate",
-    "UserTenantRoleCreate",
-    "UserTenantRoleRead",
-    "UserTenantRoleDeleteResponse",
-    "RolePermissionCreate",
-    "RolePermissionRead",
-    "RolePermissionDeleteResponse",
-    "LoginRequest",
-    "LoginResponse",
-    "TokenUserResponse",
-    "TokenTenantResponse",
-    "TokenValidationResponse",
-    "SuperBootstrapRequest",
-    "SuperBootstrapResponse",
-    "SuperBootstrapMfaVerifyRequest",
-    "SuperBootstrapMfaVerifyResponse",
-    "SuperLoginRequest",
-    "SuperLoginResponse",
-    "BootstrapTenantRequest",
-    "BootstrapTenantResponse",
-    "ExtinguisherCreate",
-    "ExtinguisherDeleteResponse",
-    "ExtinguisherRead",
-    "ExtinguisherUpdate",
+    "PermissionCreate", "PermissionRead", "PermissionResponse", "UserCreate", "UserDeleteResponse", "UserRead", "UserUpdate", "UserActivateResponse",
+    "TenantCreate", "TenantDeleteResponse", "TenantRead", "TenantUpdate", "TenantConfigUpdate", "TenantConfigRead", "UserTenantCreate", "UserTenantRead", "UserTenantDeleteResponse",
+    "RoleCreate", "RoleDeleteResponse", "RoleRead", "RoleUpdate", "UserTenantRoleCreate", "UserTenantRoleRead", "UserTenantRoleDeleteResponse",
+    "RolePermissionCreate", "RolePermissionRead", "RolePermissionDeleteResponse", "LoginRequest", "LoginResponse", "TokenUserResponse", "TokenTenantResponse", "TokenValidationResponse",
+    "SuperBootstrapRequest", "SuperBootstrapResponse", "SuperBootstrapMfaVerifyRequest", "SuperBootstrapMfaVerifyResponse", "SuperLoginRequest", "SuperLoginResponse",
+    "BootstrapTenantRequest", "BootstrapTenantResponse", "ExtinguisherCreate", "ExtinguisherDeleteResponse", "ExtinguisherRead", "ExtinguisherUpdate", "ExtinguisherTypeCreate", "ExtinguisherTypeRead",
 ]
