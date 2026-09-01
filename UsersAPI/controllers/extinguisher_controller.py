fd5d7a3f2c9e32f7a7ae7220a9a9c2eae8770cfb
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-08-31 23:10:04 -0500

    feat: wire extinguisher Excel export controller

--- UsersAPI/controllers/extinguisher_controller.py
+++ UsersAPI/controllers/extinguisher_controller.py
@@ -2,51 +2,29 @@ from sqlalchemy.orm import Session
 
 from ..models import UserTenantDB
 from ..schemas import ExtinguisherCreate, ExtinguisherUpdate
-from ..services.extinguisher_service import (
-    create_extinguisher,
-    delete_extinguisher,
-    get_extinguisher,
-    list_extinguishers,
-    update_extinguisher,
-)
+from ..services.extinguisher_export_service import export_extinguishers
+from ..services.extinguisher_service import create_extinguisher, delete_extinguisher, get_extinguisher, list_extinguishers, update_extinguisher
 
 
-def crear_extintor(
-    datos: ExtinguisherCreate,
-    db: Session,
-    user_tenant: UserTenantDB,
-):
+def crear_extintor(datos: ExtinguisherCreate, db: Session, user_tenant: UserTenantDB):
     return create_extinguisher(datos, db, user_tenant)
 
 
-def listar_extintores(
-    db: Session,
-    tenant_id: int,
-    include_inactive: bool = False,
-):
+def listar_extintores(db: Session, tenant_id: int, include_inactive: bool = False):
     return list_extinguishers(db, tenant_id, include_inactive)
 
 
-def obtener_extintor(
-    extinguisher_id: int,
-    db: Session,
-    tenant_id: int,
-):
+def obtener_extintor(extinguisher_id: int, db: Session, tenant_id: int):
     return get_extinguisher(extinguisher_id, db, tenant_id)
 
 
-def actualizar_extintor(
-    extinguisher_id: int,
-    datos: ExtinguisherUpdate,
-    db: Session,
-    user_tenant: UserTenantDB,
-):
+def actualizar_extintor(extinguisher_id: int, datos: ExtinguisherUpdate, db: Session, user_tenant: UserTenantDB):
     return update_extinguisher(extinguisher_id, datos, db, user_tenant)
 
 
-def eliminar_extintor(
-    extinguisher_id: int,
-    db: Session,
-    user_tenant: UserTenantDB,
-):
+def eliminar_extintor(extinguisher_id: int, db: Session, user_tenant: UserTenantDB):
     return delete_extinguisher(extinguisher_id, db, user_tenant)
+
+
+def exportar_extintores(db: Session, current_user: UserTenantDB, user_tenant: UserTenantDB):
+    return export_extinguishers(db, current_user, user_tenant.tenant_id)
