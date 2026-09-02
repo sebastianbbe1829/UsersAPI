c25eccceebcd6efeb3498f9d6747e1244cdbfc27
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-02 17:31:54 -0500

    refactor: expose auth context service

--- UsersAPI/services/__init__.py
+++ UsersAPI/services/__init__.py
@@ -1,4 +1,5 @@
 from .auth_service import create_access_token, get_current_user, get_current_user_tenant, get_password_hash, login_user, oauth2_scheme, validate_token, verify_password
+from .auth_context_service import get_current_user_from_token
 from .user_service import export_users, create_user, get_user, list_users, update_user, activate_user
 from .user_tenant_role_service import list_user_roles, delete_user_role, assign_role_to_user
 from .role_permission_service import assign_permission_to_role, list_role_permissions, remove_permission_from_role
@@ -8,7 +9,7 @@ from .extinguisher_type_service import list_extinguisher_types, create_extinguis
 
 __all__ = [
     "bootstrapTenant", "list_permission", "get_permission", "create_access_token", "get_current_user", "get_current_user_tenant",
-    "get_password_hash", "login_user", "oauth2_scheme", "validate_token", "verify_password", "export_users", "create_user",
+    "get_current_user_from_token", "get_password_hash", "login_user", "oauth2_scheme", "validate_token", "verify_password", "export_users", "create_user",
     "list_users", "get_user", "update_user", "list_user_roles", "delete_user_role", "assign_role_to_user", "assign_permission_to_role",
     "list_role_permissions", "remove_permission_from_role", "activate_user", "list_extinguisher_types", "create_extinguisher_type",
 ]
