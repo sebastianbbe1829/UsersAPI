8de73c74398f9ee4e10246132e851d91f70f4804
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-03 01:40:00 -0500

    fix: detect SQLAlchemy Identity in Alembic default comparator

--- alembic/env.py
+++ alembic/env.py
@@ -93,7 +93,7 @@ def compare_server_default(
     should not require a data migration solely to change that mechanism.
     """
 
-    if isinstance(metadata_column.server_default, Identity):
+    if isinstance(metadata_column.identity, Identity):
         return False
 
     return None
