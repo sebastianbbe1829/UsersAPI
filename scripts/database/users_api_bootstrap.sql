0d5853c2281cf0271817e16465cb952b33caffe1
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-03 14:00:47 -0500

    fix: avoid privileged role attributes on existing bootstrap role

--- scripts/database/users_api_bootstrap.sql
+++ scripts/database/users_api_bootstrap.sql
@@ -22,14 +22,12 @@ BEGIN
         INHERIT
         BYPASSRLS;
     ELSE
+        -- En Neon el usuario administrativo puede no ser SUPERUSER.
+        -- Solo actualizamos atributos que puede modificar sin intentar
+        -- cambiar privilegios reservados del rol existente.
         ALTER ROLE users_api_bootstrap
         LOGIN
-        PASSWORD '${USERS_API_BOOTSTRAP_PASSWORD}'
-        NOSUPERUSER
-        NOCREATEDB
-        NOCREATEROLE
-        INHERIT
-        BYPASSRLS;
+        PASSWORD '${USERS_API_BOOTSTRAP_PASSWORD}';
     END IF;
 END
 $$;
