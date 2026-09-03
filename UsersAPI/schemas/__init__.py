924f30c71cdcf48e8083d9dedebb690aeee76f1a
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-03 00:17:03 -0500

    style: format schema exports for Ruff

--- UsersAPI/schemas/__init__.py
+++ UsersAPI/schemas/__init__.py
@@ -1,15 +1,45 @@
 from .user import UserCreate, UserDeleteResponse, UserRead, UserUpdate, UserActivateResponse
-from .tenant import TenantCreate, TenantDeleteResponse, TenantRead, TenantUpdate, BootstrapTenantRequest, BootstrapTenantResponse
+from .tenant import (
+    TenantCreate,
+    TenantDeleteResponse,
+    TenantRead,
+    TenantUpdate,
+    BootstrapTenantRequest,
+    BootstrapTenantResponse,
+)
 from .tenant_config import TenantConfigUpdate, TenantConfigRead
 from .user_tenant import UserTenantCreate, UserTenantRead, UserTenantDeleteResponse
 from .role import RoleCreate, RoleDeleteResponse, RoleRead, RoleUpdate
 from .user_tenant_role import UserTenantRoleCreate, UserTenantRoleRead, UserTenantRoleDeleteResponse
 from .role_permission import RolePermissionCreate, RolePermissionRead, RolePermissionDeleteResponse
-from .auth import LoginRequest, LoginResponse, TokenUserResponse, TokenTenantResponse, TokenValidationResponse
-from .global_auth import SuperBootstrapMfaVerifyRequest, SuperBootstrapMfaVerifyResponse, SuperBootstrapRequest, SuperBootstrapResponse, SuperLoginRequest, SuperLoginResponse
-from .password_recovery import PasswordRecoveryRequest, PasswordRecoveryResponse, PasswordResetRequest, PasswordResetResponse
+from .auth import (
+    LoginRequest,
+    LoginResponse,
+    TokenUserResponse,
+    TokenTenantResponse,
+    TokenValidationResponse,
+)
+from .global_auth import (
+    SuperBootstrapMfaVerifyRequest,
+    SuperBootstrapMfaVerifyResponse,
+    SuperBootstrapRequest,
+    SuperBootstrapResponse,
+    SuperLoginRequest,
+    SuperLoginResponse,
+)
+from .password_recovery import (
+    PasswordRecoveryRequest,
+    PasswordRecoveryResponse,
+    PasswordResetRequest,
+    PasswordResetResponse,
+)
 from .permission import PermissionCreate, PermissionRead, PermissionResponse
-from .extinguisher import ExtinguisherCreate, ExtinguisherDeleteResponse, ExtinguisherRead, ExtinguisherUpdate
+from .extinguisher import (
+    ExtinguisherCreate,
+    ExtinguisherDeleteResponse,
+    ExtinguisherRead,
+    ExtinguisherUpdate,
+)
 from .extinguisher_type import ExtinguisherTypeCreate, ExtinguisherTypeRead, ExtinguisherTypeUpdate
 from .extinguisher_inspection import (
     ExtinguisherInspectionCreate,
@@ -21,13 +51,64 @@ from .extinguisher_inspection import (
 from .otp import OTPGenerateRequest, OTPGenerateResponse, OTPValidateRequest, OTPValidateResponse
 
 __all__ = [
-    "PermissionCreate", "PermissionRead", "PermissionResponse", "UserCreate", "UserDeleteResponse", "UserRead", "UserUpdate", "UserActivateResponse",
-    "TenantCreate", "TenantDeleteResponse", "TenantRead", "TenantUpdate", "TenantConfigUpdate", "TenantConfigRead", "UserTenantCreate", "UserTenantRead", "UserTenantDeleteResponse",
-    "RoleCreate", "RoleDeleteResponse", "RoleRead", "RoleUpdate", "UserTenantRoleCreate", "UserTenantRoleRead", "UserTenantRoleDeleteResponse",
-    "RolePermissionCreate", "RolePermissionRead", "RolePermissionDeleteResponse", "LoginRequest", "LoginResponse", "TokenUserResponse", "TokenTenantResponse", "TokenValidationResponse",
-    "SuperBootstrapRequest", "SuperBootstrapResponse", "SuperBootstrapMfaVerifyRequest", "SuperBootstrapMfaVerifyResponse", "SuperLoginRequest", "SuperLoginResponse",
-    "PasswordRecoveryRequest", "PasswordRecoveryResponse", "PasswordResetRequest", "PasswordResetResponse",
-    "BootstrapTenantRequest", "BootstrapTenantResponse", "ExtinguisherCreate", "ExtinguisherDeleteResponse", "ExtinguisherRead", "ExtinguisherUpdate", "ExtinguisherTypeCreate", "ExtinguisherTypeRead", "ExtinguisherTypeUpdate",
-    "ExtinguisherInspectionCreate", "ExtinguisherInspectionItemRead", "ExtinguisherInspectionRead", "ExtinguisherInspectionResultCreate", "ExtinguisherInspectionResultRead",
-    "OTPGenerateRequest", "OTPGenerateResponse", "OTPValidateRequest", "OTPValidateResponse",
+    "PermissionCreate",
+    "PermissionRead",
+    "PermissionResponse",
+    "UserCreate",
+    "UserDeleteResponse",
+    "UserRead",
+    "UserUpdate",
+    "UserActivateResponse",
+    "TenantCreate",
+    "TenantDeleteResponse",
+    "TenantRead",
+    "TenantUpdate",
+    "TenantConfigUpdate",
+    "TenantConfigRead",
+    "UserTenantCreate",
+    "UserTenantRead",
+    "UserTenantDeleteResponse",
+    "RoleCreate",
+    "RoleDeleteResponse",
+    "RoleRead",
+    "RoleUpdate",
+    "UserTenantRoleCreate",
+    "UserTenantRoleRead",
+    "UserTenantRoleDeleteResponse",
+    "RolePermissionCreate",
+    "RolePermissionRead",
+    "RolePermissionDeleteResponse",
+    "LoginRequest",
+    "LoginResponse",
+    "TokenUserResponse",
+    "TokenTenantResponse",
+    "TokenValidationResponse",
+    "SuperBootstrapRequest",
+    "SuperBootstrapResponse",
+    "SuperBootstrapMfaVerifyRequest",
+    "SuperBootstrapMfaVerifyResponse",
+    "SuperLoginRequest",
+    "SuperLoginResponse",
+    "PasswordRecoveryRequest",
+    "PasswordRecoveryResponse",
+    "PasswordResetRequest",
+    "PasswordResetResponse",
+    "BootstrapTenantRequest",
+    "BootstrapTenantResponse",
+    "ExtinguisherCreate",
+    "ExtinguisherDeleteResponse",
+    "ExtinguisherRead",
+    "ExtinguisherUpdate",
+    "ExtinguisherTypeCreate",
+    "ExtinguisherTypeRead",
+    "ExtinguisherTypeUpdate",
+    "ExtinguisherInspectionCreate",
+    "ExtinguisherInspectionItemRead",
+    "ExtinguisherInspectionRead",
+    "ExtinguisherInspectionResultCreate",
+    "ExtinguisherInspectionResultRead",
+    "OTPGenerateRequest",
+    "OTPGenerateResponse",
+    "OTPValidateRequest",
+    "OTPValidateResponse",
 ]
