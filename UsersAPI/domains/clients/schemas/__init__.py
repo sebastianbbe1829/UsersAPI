2697b0e268b0cf2d811b7c47ae3f05e850d8fd36
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-04 14:43:47 -0500

    feat: export client catalog schemas

--- UsersAPI/domains/clients/schemas/__init__.py
+++ UsersAPI/domains/clients/schemas/__init__.py
@@ -1,3 +1,12 @@
+from .catalog import CityRead, CountryRead, DepartmentRead, IdentificationTypeRead
 from .client import ClientCreate, ClientRead, ClientUpdate
 
-__all__ = ["ClientCreate", "ClientRead", "ClientUpdate"]
+__all__ = [
+    "CityRead",
+    "CountryRead",
+    "DepartmentRead",
+    "IdentificationTypeRead",
+    "ClientCreate",
+    "ClientRead",
+    "ClientUpdate",
+]
