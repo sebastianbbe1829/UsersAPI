a49c685ce5e88471e786ca2ba39eba795a6d99a7
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-04 13:26:50 -0500

    refactor: move existing layers under core domain

---
+++ UsersAPI/domains/__init__.py
@@ -0,0 +1,17 @@
+import warnings
+
+warnings.filterwarnings(
+    "ignore",
+    category=DeprecationWarning,
+    message=".*Accessing argon2.__version__ is deprecated.*",
+)
+warnings.filterwarnings(
+    "ignore",
+    category=DeprecationWarning,
+    message=".*Using `httpx` with `starlette.testclient` is deprecated.*",
+)
+warnings.filterwarnings(
+    "ignore",
+    category=DeprecationWarning,
+    message=".*starlette.testclient.*deprecated.*",
+)
