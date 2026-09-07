aecd193b1c66d512c9b198334baba8a634a6f29a
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-07 15:36:22 -0500

    feat: export client screening routes

--- UsersAPI/domains/clients/routes/__init__.py
+++ UsersAPI/domains/clients/routes/__init__.py
@@ -1,4 +1,5 @@
 from .catalog_routes import catalog_routes
 from .client_routes import client_routes
+from .screening_routes import screening_routes
 
-__all__ = ["catalog_routes", "client_routes"]
+__all__ = ["catalog_routes", "client_routes", "screening_routes"]
