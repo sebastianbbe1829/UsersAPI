757e58adb13bb652966d8e08032503e03438ec84
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-07 21:30:48 -0500

    feat(inventory): add domain repositories

---
+++ UsersAPI/domains/inventory/repositories/__init__.py
@@ -0,0 +1,11 @@
+from .inventory_movement_repository import InventoryMovementRepository
+from .inventory_repository import InventoryRepository
+from .inventory_type_repository import InventoryTypeRepository
+from .product_repository import ProductRepository
+
+__all__ = [
+    "InventoryMovementRepository",
+    "InventoryRepository",
+    "InventoryTypeRepository",
+    "ProductRepository",
+]
