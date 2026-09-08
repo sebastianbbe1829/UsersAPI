0cb53a024e3ec6ac5bdaad55cee7cacc49f84c68
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-08 00:43:25 -0500

    feat(sales): add sale schemas

---
+++ UsersAPI/domains/sales/schemas/__init__.py
@@ -0,0 +1,21 @@
+from .sale import (
+    SaleCreate,
+    SaleCustomerCreate,
+    SaleCustomerRead,
+    SaleItemCreate,
+    SaleItemRead,
+    SalePaymentCreate,
+    SalePaymentRead,
+    SaleRead,
+)
+
+__all__ = [
+    "SaleCreate",
+    "SaleCustomerCreate",
+    "SaleCustomerRead",
+    "SaleItemCreate",
+    "SaleItemRead",
+    "SalePaymentCreate",
+    "SalePaymentRead",
+    "SaleRead",
+]
