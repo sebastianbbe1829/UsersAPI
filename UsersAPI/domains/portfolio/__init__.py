7d72e9c52aa0e225c0226ceec7c041ece643bd77
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-08 13:17:25 -0500

    feat(portfolio): create cartera domain skeleton

---
+++ UsersAPI/domains/portfolio/__init__.py
@@ -0,0 +1,8 @@
+from .models import CreditLimitDB, ObligationDB, PaymentAllocationDB, PaymentDB
+
+__all__ = [
+    "CreditLimitDB",
+    "ObligationDB",
+    "PaymentDB",
+    "PaymentAllocationDB",
+]
