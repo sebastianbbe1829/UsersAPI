1f4388649272022edbf7a9387d799192f2358f93
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-08 13:18:47 -0500

    feat(portfolio): add cartera service exports

---
+++ UsersAPI/domains/portfolio/services/__init__.py
@@ -0,0 +1,17 @@
+from .portfolio_service import (
+    get_client_credit,
+    list_client_obligations,
+    list_obligations,
+    list_payments,
+    register_payment,
+    upsert_client_credit_limit,
+)
+
+__all__ = [
+    "get_client_credit",
+    "upsert_client_credit_limit",
+    "list_obligations",
+    "list_client_obligations",
+    "register_payment",
+    "list_payments",
+]
