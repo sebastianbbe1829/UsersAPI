5ccbc30bfde83ea65496c900d2b91c09f0d4144c
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-08 13:19:03 -0500

    feat(portfolio): add cartera controllers

---
+++ UsersAPI/domains/portfolio/controllers/__init__.py
@@ -0,0 +1,17 @@
+from .portfolio_controller import (
+    client_credit,
+    client_obligations,
+    create_payment,
+    obligations,
+    payments,
+    update_client_credit,
+)
+
+__all__ = [
+    "client_credit",
+    "update_client_credit",
+    "obligations",
+    "client_obligations",
+    "create_payment",
+    "payments",
+]
