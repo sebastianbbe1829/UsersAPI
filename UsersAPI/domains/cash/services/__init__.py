629dc36381de5d2da937789320dbb0f6642aa699
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-10 01:33:32 -0500

    feat(cash): export operating day service

--- UsersAPI/domains/cash/services/__init__.py
+++ UsersAPI/domains/cash/services/__init__.py
@@ -10,11 +10,18 @@ from .cash_admin_service import (
     update_cash_box,
 )
 from .cash_context_service import get_user_cash_context
+from .cash_day_service import close_branch, close_day, close_register, get_current_day, serialize_day, start_day
 from .cash_service import CashService
 
 __all__ = [
     "CashService",
     "get_user_cash_context",
+    "get_current_day",
+    "start_day",
+    "close_register",
+    "close_branch",
+    "close_day",
+    "serialize_day",
     "list_branches",
     "create_branch",
     "update_branch",
