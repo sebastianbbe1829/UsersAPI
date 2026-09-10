6e153ced8c9a5970f0c297bce3889871163f5541
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-09 21:54:17 -0500

    feat(cash): add cash controllers

---
+++ UsersAPI/domains/cash/controllers/__init__.py
@@ -0,0 +1,11 @@
+from .cash_controller import add_movement, close_register, current_register, get_register, list_registers, open_register, register_summary
+
+__all__ = [
+    "open_register",
+    "current_register",
+    "list_registers",
+    "get_register",
+    "add_movement",
+    "close_register",
+    "register_summary",
+]
