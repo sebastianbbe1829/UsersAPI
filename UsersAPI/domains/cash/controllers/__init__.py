0518dac69fb518398fe0cb21fc30ee970dd0a80c
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-09 21:55:04 -0500

    style(cash): format controller exports

--- UsersAPI/domains/cash/controllers/__init__.py
+++ UsersAPI/domains/cash/controllers/__init__.py
@@ -1,4 +1,12 @@
-from .cash_controller import add_movement, close_register, current_register, get_register, list_registers, open_register, register_summary
+from .cash_controller import (
+    add_movement,
+    close_register,
+    current_register,
+    get_register,
+    list_registers,
+    open_register,
+    register_summary,
+)
 
 __all__ = [
     "open_register",
