a736627fc7a3a4e5c81e5ce63940a6ceb67fb388
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-10 00:16:21 -0500

    feat(cash): export administration service

--- UsersAPI/domains/cash/services/__init__.py
+++ UsersAPI/domains/cash/services/__init__.py
@@ -1,4 +1,27 @@
+from .cash_admin_service import (
+    create_assignment,
+    create_branch,
+    create_cash_box,
+    list_assignments,
+    list_branches,
+    list_cash_boxes,
+    unassign,
+    update_branch,
+    update_cash_box,
+)
 from .cash_context_service import get_user_cash_context
 from .cash_service import CashService
 
-__all__ = ["CashService", "get_user_cash_context"]
+__all__ = [
+    "CashService",
+    "get_user_cash_context",
+    "list_branches",
+    "create_branch",
+    "update_branch",
+    "list_cash_boxes",
+    "create_cash_box",
+    "update_cash_box",
+    "list_assignments",
+    "create_assignment",
+    "unassign",
+]
