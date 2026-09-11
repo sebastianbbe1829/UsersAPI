8ffc1707b7c25b57496aa8c58ecb9193c5de00e1
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-10 22:32:40 -0500

    fix(alembic): use unique merge revision for cash traceability heads

--- alembic/versions/a7b8c9d0e1f2_merge_cash_traceability_heads.py
+++ alembic/versions/a7b8c9d0e1f2_merge_cash_traceability_heads.py
@@ -1,13 +1,13 @@
 """merge cash traceability migration heads
 
-Revision ID: a7b8c9d0e1f2
+Revision ID: c9d8e7f6a5b4
 Revises: d5e6f7a8b9c0, f4c8a1b2d3e4
 Create Date: 2026-09-10
 """
 
 from typing import Sequence, Union
 
-revision: str = "a7b8c9d0e1f2"
+revision: str = "c9d8e7f6a5b4"
 down_revision: Union[str, Sequence[str], None] = (
     "d5e6f7a8b9c0",
     "f4c8a1b2d3e4",
