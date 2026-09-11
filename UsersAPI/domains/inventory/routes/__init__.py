3089931fe407209708e0390fe5ab08b3f3911995
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-11 14:54:04 -0500

    Register inventory movement lookup routes

--- UsersAPI/domains/inventory/routes/__init__.py
+++ UsersAPI/domains/inventory/routes/__init__.py
@@ -1,3 +1,6 @@
 from .inventory_routes import inventory_routes
+from .movement_lookup_routes import movement_lookup_routes
 
-__all__ = ["inventory_routes"]
+inventory_routes.include_router(movement_lookup_routes)
+
+__all__ = ["inventory_routes", "movement_lookup_routes"]
